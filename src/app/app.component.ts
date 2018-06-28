c39a3fc3536bc71ace44e440fba8b326ddeee478
DESKTOP-AGO6J1R\bvrs9 <bvrs95@gmail.com>
2018-06-28 09:02:44 +0530

    updated header as per mocks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { of } from 'rxjs/observable/of';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   summary;
